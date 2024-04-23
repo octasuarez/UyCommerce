42389bb17aeb58fbd5b34bd648de5d67f7ae5bf7
Octavio Suarez <octasuarezp@gmail.com>
2024-04-23 08:22:51 -0300

    Crud Marcas completo

--- UYCommerce/Controllers/MarcaController.cs
+++ UYCommerce/Controllers/MarcaController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UYCommerce.Data;
@@ -33,13 +34,80 @@ namespace UYCommerce.Controllers
             var marcas = await _context.Marcas
                 .Include(m => m.Productos)
                 .ToListAsync();
-            return View("../Admin/GetMarcas",marcas);
+            return View("../Admin/GetMarcas", marcas);
         }
 
         [HttpGet]
         public async Task<IActionResult> GetMarcasApi()
         {
             return Ok(await _context.Marcas.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult CreateMarca()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "Admin")]
+        public async Task<IActionResult> CreateMarca(string nombre)
+        {
+
+            var error = await MarcaIsValid(nombre);
+            if (error is not null) {
+                return BadRequest(new { error });
+            }
+
+            _context.Marcas.Add(new Marca { Nombre = nombre });
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        private async Task<string?> MarcaIsValid(string nombre)
+        {
+
+            if (string.IsNullOrEmpty(nombre))
+            {
+                return "Campo obligatorio";
+            }
+
+            var marca = await _context.Marcas.FirstOrDefaultAsync(m => m.Nombre!.ToLower() == nombre.ToLower());
+
+            if (marca is not null)
+            {
+                return "Marca ya existe";
+            }
+
+            return null;
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "Admin")]
+        public async Task<IActionResult> DeleteMarca(int marcaId) {
+
+
+            var marca = await GetMarcaById(marcaId);
+
+            if(marca is null) {
+                return BadRequest("Marca no econtrada");
+            }
+
+            _context.Remove(marca);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<Marca?> GetMarcaById(int id) {
+
+            var marca = await _context.Marcas
+                .Include(m => m.Productos)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            return marca;
         }
     }
 }
